4626db0654732c87dd256cf855db2084ab3884f1
MingxunBai <MingxunBai@outlook.com>
2016-02-15 11:45:19 +0800

    init.el

--- init.el
+++ init.el
@@ -18,9 +18,6 @@
 ;; 支持和外部程序的拷贝
 (setq x-select-enable-clipboard t)
 
-;; 设置默认模式为 text
-(setq default-major-mode 'text-mode)
-
 ;; 匹配括号高亮
 (show-paren-mode t)
 (setq show-paren-style 'parentheses)
@@ -31,49 +28,20 @@
 ;; 设置回车后自动换行
 (global-set-key (kbd "RET") 'newline-and-indent)
 
+;; 去掉滚动条
+(set-scroll-bar-mode nil)
+
 ;; 设置 tab
-(defun my-setup-indent (n)
+(setq c-indent-level 4)
+(setq c-basic-offset 4)
 
-  ;; java/c/c++
-  (setq c-basic-offset n)
+;; set Emmet
+(add-to-list 'load-path "~/.emacs.d/plugins/emmet-mode")
+(require 'emmet-mode)
+(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ; indent 2 space
 
-  ;; web development
-  (setq coffee-tab-width n) ; coffeescript
-  (setq javascript-indent-level n) ; javascript-mode
-  (setq js-indent-level n) ; js-mode
-  (setq js2-basic-offset n) ; js2-mode, inlatest js2-mode, it's alias of js-indent-level
-  (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
-  (setq web-mode-css-indent-offset n) ; web-mode, css in html file
-  (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
-  (setq css-indent-offset n) ; css-mode
-)
+(add-hook 'css-mode-hook 'emmet-mode) ; enable Emmet's css abbreviation
+(global-set-key (kbd "C-x C-_") 'emmet-mode)
 
-(defun my-office-code-style ()
-  (interactive)
-  (message "Office code style!")
-  (setq indent-tabs-mode t) ; use tab instead of space
-  (my-setup-indent 4) ; indnet 4 space width
-)
-
-(defun my-personal-code-style ()
-  (interactive)
-  (message "My personal code style!")
-  (setq indent-tabs-mode nil) ; use space instead of tab
-  (my-setup-indnet 2) ; indent 2 space width
-)
-
-(defun my-setup-develop-environmnet ()
-  (interactive)
-  (let ((proj-dir (file-name-directory (buffer-file-name))))
-    ;; if hobby project path contains string "hobby-proj1"
-    (if (string-match-p "hobby-proj1" proj-dir)
-	(my-personal-code-style))
-
-    ;; if commericial project path contains string "commerical-proj"
-    (if (string-match-p "commerical-proj" proj-dir)
-	(my-office-code-style))
-  )
-)
-
-;; prog-mode-hock requires emacs24+
-(add-hook 'prog-mode-hock 'my-setup-develop-environment)
+;; set auto-complete
+(add-to-list 'load-path "~/.emacs.d/plugins/auto-complete")
