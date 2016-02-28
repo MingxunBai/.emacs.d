31f8b71bca0e4090eb155283e4f57136829e2951
Jiangty <MingxunBai@outlook.com>
2016-02-28 16:50:31 +0800

    Update init.el

--- init.el
+++ init.el
@@ -2,6 +2,10 @@
 (setq user-full-name "MingxunBai")
 (setq user-mail-address "MingxunBai@outlook.com")
 
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'init-site-lisp)
+(require 'init-elpa)
+
 (setq inhibit-startup-message t) ; 关闭启动动画
 
 (setq kill-ring-max 500) ; 设置历史记录数量
@@ -14,22 +18,5 @@
 (set-scroll-bar-mode nil)
 (tool-bar-mode -1)
 
-;; set Emmet
-(require-package 'emmet-mode)
-(require 'emmet-mode)
-(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ; indent 2 space
-(add-hook 'css-mode-hook 'emmet-mode) ; enable Emmet's css abbreviation
-(global-set-key (kbd "\C-x \C-_") 'emmet-mode)
-
-;; set auto-complete
-(require-package 'auto-complete)
-(require 'auto-complete-config)
-(global-auto-complete-mode t)
-(setq tab-always-indent 'complete) ; indent first, then complete
-(setq ac-auto-start 1)
-; (setq-default ac-expand-on-auto-complete nil) ; stop auto-start
-(ac-set-trigger-key "TAB") ; use tab to complete
-(setq ac-auto-show-menu 0.2)
-(setq ac-use-menu-map t) ; set menu hotkey
-(define-key ac-menu-map "\C-n" 'ac-next)
-(define-key ac-menu-map "\C-p" 'ac-previous)
+(require 'init-emmet-mode)
+(require 'init-auto-complete)
