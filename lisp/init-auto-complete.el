9b27412ca82fc7c97819a690c9c8ab4c9a8ca31e
Jiangty <MingxunBai@outlook.com>
2016-03-02 16:40:59 +0800

    Update

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -2,10 +2,9 @@
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
 (setq tab-always-indent 'complete) ; indent first, then complete
-(setq ac-auto-start 1)
-; (setq-default ac-expand-on-auto-complete nil) ; stop auto-start
+(setq-default ac-auto-start nil)
+(setq-default ac-expand-on-auto-complete nil)
 (ac-set-trigger-key "TAB") ; use tab to complete
-(setq ac-auto-show-menu 0.2)
 (setq ac-use-menu-map t) ; set menu hotkey
 (define-key ac-menu-map "\C-n" 'ac-next)
 (define-key ac-menu-map "\C-p" 'ac-previous)
