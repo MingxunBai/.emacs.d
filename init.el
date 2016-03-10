ef61a7f0f98bca011f0b0d7109cd74ab2758c0e1
MingxunBai <MingxunBai@users.noreply.github.com>
2016-03-10 14:11:09 +0800

    Update init.el

--- init.el
+++ init.el
@@ -24,15 +24,15 @@
 (setq ido-save-directory-list-file nil)
 
 (show-paren-mode t)
-(setq show-paren-style 'parenthesis) ; 括号匹配时可以高亮显示另外一边的括号，但光标不会烦人的跳到另一个括号处
+(setq show-paren-style 'parenthesis) ; 括号匹配时可以高亮显示另外一边的括号，但光标不会跳到另一个括号处
 
 (setq-default kill-whole-line t) ; 在行首 C-k 时，同时删除该行
 
 (setq track-eol t) ; 当光标在行尾上下移动的时候，始终保持在行尾
 
-(setq scroll-margin 3 scroll-conservatively 10000) ; 防止页面滚动时跳动， scroll-margin 3 可以在靠近屏幕边沿3行时就开始滚动，可以很好的看到上下文
+(setq scroll-margin 3 scroll-conservatively 10000) ; 防止页面滚动时跳动，scroll-margin 3 可以在靠近屏幕边沿3行时就开始滚动，可以很好的看到上下文
 
-(fset 'yes-or-no-p 'y-or-n-p)
+(fset 'yes-or-no-p 'y-or-n-p) ; 使用 y/n 替代 yes/no
 
 (setq x-select-enable-clipboard t) ; 支持和外部程序的拷贝
 
@@ -42,13 +42,6 @@
 (tool-bar-mode -1) ; 隐藏工具栏
 
 (global-set-key (kbd"RET") 'newline-and-indent) ; 回车时缩进
-
-(global-set-key (kbd "s-SPC") 'set-mark-command) ; 用 win+space 键来 set-mark
-
-(setq auto-mode-alist
-      (append '(("\\.css\\'" . css-mode)
-		("\\.js\\'" . javascript-mode))
-	      auto-mode-alist))
 
 ;; Auto-complete
 (require 'auto-complete-config)
@@ -63,19 +56,9 @@
 
 ;; Emmet-mode
 (require 'emmet-mode)
-(add-hook 'web-mode-hook (lambda ()
-			   (emmet-mode t)))
 
 ;; Web-mode
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 ;; highlight-parentheses-mode
 (require 'highlight-parentheses)
@@ -85,6 +68,24 @@
     (highlight-parentheses-mode t))) 
 (global-highlight-parentheses-mode)
 
+;; 自加载对应模式
+(setq auto-mode-alist
+      (append '(("\\.css\\'" . css-mode)
+		("\\.js\\'" . javascript-mode)
+		("\\.phtml\\'" . web-mode)
+		("\\.tpl\\.php\\'" . web-mode)
+		("\\.[agj]sp\\'" . web-mode)
+		("\\.as[cp]x\\'" . web-mode)
+		("\\.erb\\'" . web-mode)
+		("\\.mustache\\'" . web-mode)
+		("\\.djhtml\\'" . web-mode)
+		("\\.html?\\'" . web-mode))
+	      auto-mode-alist))
+(add-hook 'web-mode-hook (lambda ()
+			   (emmet-mode t)))
+(add-hook 'css-mode-hook (lambda ()
+			   (emmet-mode t)))
+			   
 ;;显示时间设置
 (display-time-mode 1) ; 启用时间显示设置，在minibuffer上面的那个杠上
 (setq display-time-24hr-format t) ; 时间使用24小时制
