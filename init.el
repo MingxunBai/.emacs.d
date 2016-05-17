8be726ded5b9292387d2d04854f2327cd66f6425
Mingxunbai <Mingxunbai@outlook.com>
2016-05-17 13:33:58 +0800

    Update

--- init.el
+++ init.el
@@ -33,10 +33,6 @@
 (setq column-number-mode t)
 (setq line-number-mode t)
 
-;; Ido-mode
-(ido-mode t)
-(setq ido-save-directory-list-file nil)
-
 (show-paren-mode t)
 (setq show-paren-style 'parenthesis) ; 括号匹配时可以高亮显示另外一边的括号，但光标不会跳到另一个括号处
 
@@ -44,7 +40,7 @@
 
 (setq track-eol t) ; 当光标在行尾上下移动的时候，始终保持在行尾
 
-(setq scroll-margin 3 scroll-conservatively 10000) ; 防止页面滚动时跳动，scroll-margin 3 可以在靠近屏幕边沿3行时就开始滚动，可以很好的看到上下文
+(setq scroll-margin 3 scroll-conservatively 10000) ; 靠近屏幕边沿3行时就开始滚动
 
 (fset 'yes-or-no-p 'y-or-n-p) ; 使用 y/n 替代 yes/no
 
@@ -58,6 +54,19 @@
 
 (global-set-key (kbd"RET") 'newline-and-indent) ; 回车时缩进
 (setq-default indent-tabs-mode  nil) ; 设置缩进为空格
+
+(display-time-mode 1) ; 启用时间显示
+(setq display-time-24hr-format t) ; 24小时制
+
+;; Ido-mode
+(ido-mode t)
+(setq ido-save-directory-list-file nil)
+
+;; Org-mode 自动换行
+(add-hook 'org-mode-hook
+          (lambda ()
+            (setq truncate-lines nil)
+            (set-fill-column 70)))
 
 ;; Auto-complete
 (require 'auto-complete-config)
@@ -79,11 +88,17 @@
 (add-hook 'css-mode-hook (lambda ()
 			   (emmet-mode t)))
 
-;; org-mode 自动换行
-(add-hook 'org-mode-hook
-          (lambda ()
-            (setq truncate-lines nil)
-            (set-fill-column 70)))
+;; Highlight-parentheses-mode
+(require 'highlight-parentheses)
+(define-globalized-minor-mode global-highlight-parentheses-mode
+  highlight-parentheses-mode
+  (lambda ()
+    (highlight-parentheses-mode t))) 
+(global-highlight-parentheses-mode)
+
+;; Windows numbering
+(require 'window-numbering)
+(window-numbering-mode 1)
 
 ;; Web-mode
 (require 'web-mode)
@@ -92,14 +107,6 @@
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2))
 (add-hook 'web-mode-hook  'my-web-mode-hook)
-
-;; highlight-parentheses-mode
-(require 'highlight-parentheses)
-(define-globalized-minor-mode global-highlight-parentheses-mode
-  highlight-parentheses-mode
-  (lambda ()
-    (highlight-parentheses-mode t))) 
-(global-highlight-parentheses-mode)
 
 ;; YASnippet
 (require 'yasnippet)
@@ -129,7 +136,7 @@
         (yas-expand)))))
 (define-key yas-minor-mode-map (kbd "<C-tab>") 'yas-ido-expand)
 
-;; use popup menu for yas-choose-value
+;; Use popup menu for yas-choose-value
 (require 'popup)
 (defun yas-popup-isearch-prompt (prompt choices &optional display-fn)
   (when (featurep 'popup)
@@ -162,7 +169,3 @@
 		("\\.txt\\'" . org-mode)
 		("\\.py\\'" . python-mode))
 	      auto-mode-alist))
-
-;; 显示时间设置
-(display-time-mode 1) ; 启用时间显示设置
-(setq display-time-24hr-format t) ; 时间使用24小时制
