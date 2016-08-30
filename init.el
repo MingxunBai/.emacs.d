5f2e84fa80701e96cdc88e0c3bacb3f036b9bcbb
Mingxunbai <Mingxunbai@outlook.com>
2016-08-30 16:11:48 +0800

    Update

--- init.el
+++ init.el
@@ -13,9 +13,9 @@
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'gb18030)
+;; (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
 
 ;; 显示
 (setq inhibit-startup-message t) ; 关闭启动动画
