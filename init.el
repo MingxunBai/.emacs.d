417b8296ba5e66d52cdb88a62c9384f7af0945f3
MingxunBai <mingxunbai@outlook.com>
2016-09-09 15:19:58 +0800

    Update

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;;-------------------------------------------------
 ;; basic setting
 ;;-------------------------------------------------
+
 ;; 设置个人信息
 (setq user-full-name "MingxunBai"
       user-mail-address "mingxunbai@outlook.com")
@@ -11,11 +12,13 @@
 ;; 路径配置
 (add-to-list 'load-path (expand-file-name "plugins" user-emacs-directory))
 
-(when *Windows* (setq default-directory "c:\\xampp\\htdocs"))
+(when *Windows*
+  (setq default-directory "c:\\xampp\\htdocs"))
 
 ;;-------------------------------------------------
 ;; 编码环境
 ;;-------------------------------------------------
+
 (setq current-language-environment "UTF-8")
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
@@ -27,20 +30,22 @@
 ;;-------------------------------------------------
 ;; 显示
 ;;-------------------------------------------------
-(setq inhibit-startup-message t) ; 关闭启动动画
 
-(setq split-height-threshold nil ; 垂直分屏
+(setq inhibit-startup-message t)        ; 关闭启动动画
+
+(setq split-height-threshold nil        ; 垂直分屏
       split-width-threshold 0)
 
 (setq frame-title-format '("Emacs@%S" (buffer-file-name "%f" (dired-directory dired-directory "%b")))) ; 标题显示完整路径
 
 ;; 设置字体
 ;; (set-default-font "Source Code Pro-12")
-(when *Windows* (set-fontset-font t 'han (font-spec :family "Minglan_Code"))) ; 修改 windows 下的中文字体为 "明兰黑"
+(when *Windows*
+  (set-fontset-font t 'han (font-spec :family "Minglan_Code")))    ; 修改 windows 下的中文字体为 "明兰黑"
 
 ;; 语法高亮
 (global-font-lock-mode t)
-(setq font-lock-maximum-decoration t ; 只渲染当前 buffer 语法高亮
+(setq font-lock-maximum-decoration t    ; 只渲染当前 buffer 语法高亮
       font-lock-verbose t
       font-lock-maximum-size '((t . 1048576) (vm-mode . 5250000)))
 
@@ -50,41 +55,41 @@
       column-number-mode t
       line-number-mode t)
 
-(show-paren-mode t) ; 高亮匹配括号
-(setq show-paren-style 'parenthesis) ; 光标不会跳到另一个括号处
+(show-paren-mode t)                     ; 高亮匹配括号
+(setq show-paren-style 'parenthesis)    ; 光标不会跳到另一个括号处
 
-(setq scroll-margin 3 scroll-conservatively 10000) ; 靠近屏幕边沿3行时就开始滚动
+(setq scroll-margin 3                   ; 靠近屏幕边沿3行时就开始滚动
+      scroll-conservatively 10000)
 
-(scroll-bar-mode -1) ; 隐藏滚动条
-(tool-bar-mode -1) ; 隐藏工具栏
-(menu-bar-mode -1) ; 隐藏菜单栏
+(scroll-bar-mode -1)                    ; 隐藏滚动条
+(tool-bar-mode -1)                      ; 隐藏工具栏
+(menu-bar-mode -1)                      ; 隐藏菜单栏
 
-(setq display-time-24hr-format t) ; 24小时制
-(display-time) ; 启用时间显示
+(setq display-time-24hr-format t)       ; 24小时制
+(display-time)                          ; 启用时间显示
 
 ;;-------------------------------------------------
 ;; 操作
 ;;-------------------------------------------------
-(setq default-major-mode 'text-mode) ; 设置默认主模式为 text-mode
+(setq default-major-mode 'text-mode)    ; 设置默认主模式为 text-mode
 
-(setq kill-ring-max 500) ; 设置历史记录数量
+(setq kill-ring-max 500)                ; 设置历史记录数量
 
-(setq inhibit-startup-message t) ; 关闭出错提示音
+(setq inhibit-startup-message t)        ; 关闭出错提示音
 
-;; (setq-default kill-whole-line t) ; 在行首 C-k 时，同时删除该行
+(setq-default kill-whole-line t)        ; 在行首 C-k 时，同时删除该行
 
-(setq track-eol t) ; 当光标在行尾上下移动的时候，始终保持在行尾
+(setq track-eol t)                      ; 当光标在行尾上下移动的时候，始终保持在行尾
 
-(fset 'yes-or-no-p 'y-or-n-p) ; 使用 y/n 替代 yes/no
+(fset 'yes-or-no-p 'y-or-n-p)           ; 使用 y/n 替代 yes/no
 
-(setq x-select-enable-clipboard t) ; 支持和外部程序的拷贝
+(setq x-select-enable-clipboard t)      ; 支持和外部程序的拷贝
 
-(setq make-backup-files nil ; 不生成备份文件
-      auto-save-default nil) ; 不生成临时文件
+(setq make-backup-files nil             ; 不生成备份文件
+      auto-save-default nil)            ; 不生成临时文件
 
-(global-set-key (kbd "RET") 'newline-and-indent) ; 回车时缩进
-
-(global-set-key (kbd "C-c k") 'kill-buffer-and-window) ; C-c k 删除 buffer 和 window
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-c k") 'kill-buffer-and-window)
 
 ;; 缩进回退
 (defun un-indent-by-removing-4-spaces ()
@@ -93,19 +98,22 @@
       (let ((mark (mark)))
         (save-excursion
           (save-match-data
-            (indent-rigidly (region-beginning)
-                            (region-end)
-                            -4)
+            (indent-rigidly
+             (region-beginning)
+             (region-end)
+             -4)
             (push-mark mark t t)
             (setq deactivate-mark nil))))
-    (indent-rigidly (line-beginning-position)
-                    (line-end-position)
-                    -4)))
-(global-set-key (kbd "<backtab>") 'un-indent-by-removing-4-spaces) ; 绑定 S-tab 回退缩进
+    (indent-rigidly
+     (line-beginning-position)
+     (line-end-position)
+     -4)))
+(global-set-key (kbd "<backtab>") 'un-indent-by-removing-4-spaces)
 
-(setq-default indent-tabs-mode  nil ; 设置缩进为空格
-              default-tab-width 4 ; 设置默认缩进为 4
-              c-basic-offset 4) ; 修改 C 语言缩进为 4
+;; 设置缩进
+(setq-default indent-tabs-mode  nil     ; 设置缩进为空格
+              default-tab-width 4       ; 设置默认缩进为 4
+              c-basic-offset 4)         ; 修改 C 语言缩进为 4
 
 ;; 向上新建一行
 (defun up-newline ()
@@ -115,7 +123,7 @@
     (newline-and-indent)
     (previous-line)
     (indent-according-to-mode)))
-(global-set-key (kbd "C-c p") 'up-newline)
+(global-set-key (kbd "M-O") 'up-newline)
 
 ;; 向下新建一行
 (defun down-newline ()
@@ -123,7 +131,7 @@
   (progn
     (end-of-line)
     (newline-and-indent)))
-(global-set-key (kbd "C-c n") 'down-newline)
+(global-set-key (kbd "M-o") 'down-newline)
 
 ;; 自动匹配括号
 (setq skeleton-pair-alist 
@@ -149,9 +157,12 @@
 ;;-------------------------------------------------
 ;; internal mode
 ;;-------------------------------------------------
-;; emacs-lisp-mode 下禁止自动匹配 '
+
+;; emacs-lisp-mode
 (defun unable-quotation-hook ()
-  (setq skeleton-pair-alist '((?\' _ "" >))))
+  (setq skeleton-pair-alist
+        '((?\' _ "" >))))               ; 禁止 ' 自动补齐
+
 (add-hook 'emacs-lisp-mode-hook 'unable-quotation-hook)
 
 ;; hs-mode
@@ -162,58 +173,65 @@
 (setq ido-save-directory-list-file nil)
 
 ;; org-mode
-(add-hook 'org-mode-hook (lambda ()                           
-                           (setq skeleton-pair-alist '((?\[ "" >))) ; org mode 下禁止自动补齐 ]
-                           
-                           (setq org-startup-indented t) ; 自动缩进
+(defun my-org-mode-hook ()                     
+  (setq skeleton-pair-alist
+        '((?\[ "" >)))                  ; 禁止 [ 自动补齐
+  
+  (setq org-startup-indented t)         ; 自动缩进
 
-                           ;; 代码高亮
-                           (require 'htmlize)
-                           (setq org-src-fontify-natively t)
+  ;; 代码高亮
+  (require 'htmlize)
+  (setq org-src-fontify-natively t)
 
-                           (defun org-insert-src-block (src-code-type)
-                             (interactive
-                              (let ((src-code-types
-                                     '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++" "css"
-                                       "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
-                                       "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk" "ditaa"
-                                       "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
-                                       "scheme" "sqlite" "html")))
-                                (list (ido-completing-read "Source code type: " src-code-types))))
-                             (progn
-                               (newline-and-indent)
-                               (insert (format "#+BEGIN_SRC %s\n" src-code-type))
-                               (newline-and-indent)
-                               (insert "#+END_SRC\n")
-                               (previous-line 2)
-                               (org-edit-src-code)))
+  (defun org-insert-src-block (src-code-type)
+    (interactive
+     (let ((src-code-types
+            '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++" "css"
+              "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
+              "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk" "ditaa"
+              "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
+              "scheme" "sqlite" "html")))
+       (list (ido-completing-read "Source code type: " src-code-types))))
+    (progn
+      (newline-and-indent)
+      (insert (format "#+BEGIN_SRC %s\n" src-code-type))
+      (newline-and-indent)
+      (insert "#+END_SRC\n")
+      (previous-line 2)
+      (org-edit-src-code)))
 
-                           (local-set-key (kbd "C-c s e") ; keybinding for editing source code blocks
-                                          'org-edit-src-code)
-                           (local-set-key (kbd "C-c s i") ; keybinding for inserting code blocks
-                                          'org-insert-src-block)))
+  (local-set-key (kbd "C-c s e") 'org-edit-src-code)
+  (local-set-key (kbd "C-c s i") 'org-insert-src-block))
+
+(add-hook 'org-mode-hook 'my-org-mode-hook)
 
 ;; winner-mode
-(when (fboundp 'winner-mode) (winner-mode 1))
+(when (fboundp 'winner-mode)
+  (winner-mode 1))
 (global-set-key (kbd "C-x 4 u") 'winner-undo)
 (global-set-key (kbd "C-x 4 r") 'winner-redo)
 
 ;;-------------------------------------------------
 ;; extension
 ;;-------------------------------------------------
+
 ;; ac-js2
-(defun enable-ac-js2-mode ()
+(defun ac-js2-mode-hook ()
   (require 'ac-js2)
   (ac-js2-mode))
+
+(add-hook 'js2-mode-hook 'ac-js2-mode-hook)
 
 ;; auto-complete
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories (expand-file-name "plugins/auto-complete/dict" user-emacs-directory))
+;; enable
 (ac-config-default)
 (global-auto-complete-mode t)
 (add-hook 'text-mode-hook 'auto-complete-mode)
 (setq ac-auto-start nil)
-(setq ac-use-menu-map t) ; set hot key for menu map
+;; set hot key
+(setq ac-use-menu-map t)
 (define-key ac-mode-map "\M-/" 'auto-complete)
 (define-key ac-completing-map "\M-/" 'ac-stop)
 
@@ -228,8 +246,10 @@
 (defun enable-emmet-mode ()
   (require 'emmet-mode)
   (emmet-mode)
-  (global-set-key (kbd "C-M-p") 'emmet-prev-edit-point)
-  (global-set-key (kbd "C-M-n") 'emmet-next-edit-point))
+  (define-key emmet-mode-keymap (kbd "<C-M-left>") nil)
+  (define-key emmet-mode-keymap (kbd "<C-M-right>") nil)
+  (define-key emmet-mode-keymap (kbd "C-M-p") 'emmet-prev-edit-point)
+  (define-key emmet-mode-keymap (kbd "C-M-n") 'emmet-next-edit-point))
 
 ;; highlight-parentheses-mode
 (require 'highlight-parentheses)
@@ -237,6 +257,7 @@
   highlight-parentheses-mode
   (lambda ()
     (highlight-parentheses-mode t)))
+
 (global-highlight-parentheses-mode)
 
 ;; js2-mode
@@ -248,13 +269,14 @@
 (defun enable-markdown-mode ()
   (require 'markdown-mode)
   (markdown-mode)
-  (when *Windows* (custom-set-variables '(markdown-command "markdown.pl")))) ; set markdown-command name
+  (when *Windows*                       ; set markdown-command for windows
+    (custom-set-variables '(markdown-command "markdown.pl"))))
 
 ;; multiple-cursors
 (require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines) ; edit each line in region
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-unset-key (kbd "M-<down-mouse-1>"))
-(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click) ; bind mouse event
+(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
 
 ;; project-explorer
 (require 'project-explorer)
@@ -264,9 +286,13 @@
 (defun enable-web-mode ()
   (require 'web-mode)
   (web-mode)
+  
   (hs-minor-mode t)
   (enable-emmet-mode)
-  (setq skeleton-pair-alist '((?\< "" >))) ; web mode 下禁止自动补齐 >
+
+  (setq skeleton-pair-alist
+        '((?\< "" >)))                  ; 禁止 < 自动补齐
+  
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 4)
   (setq web-mode-code-indent-offset 4)
@@ -288,7 +314,6 @@
 
 ;; use popup menu for yas-choose-value
 (defun yas-popup-isearch-prompt (prompt choices &optional display-fn)
-  (require 'popup) ; use popup menu for yas-choose-value
   (when (featurep 'popup)
     (popup-menu*
      (mapcar
@@ -306,7 +331,6 @@
 
 ;; Completing point by some yasnippet key
 (defun yas-ido-expand ()
-  "Lets you select (and expand) a yasnippet key"
   (interactive)
   (let ((original-point (point)))
     (while (and
@@ -332,10 +356,10 @@
 ;;-------------------------------------------------
 ;; 指定主模式
 ;;-------------------------------------------------
+
 (setq auto-mode-alist
       (append '(("\\.js\\'" . (lambda ()
-                                (enable-js2-mode)
-                                (enable-ac-js2-mode)))
+                                (enable-js2-mode)))
                 ("\\.md\\'" . (lambda ()
                                 (enable-markdown-mode)))
                 ("\\.php\\'" . (lambda ()
