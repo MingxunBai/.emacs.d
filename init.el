57e32503f920316f6c89de07a642b8024826e0d2
MingxunBai <mingxunbai@outlook.com>
2016-11-26 17:45:12 +0800

    Update

--- init.el
+++ init.el
@@ -358,11 +358,11 @@
   (defun org-insert-src-block (src-code-type)
     (interactive
      (let ((src-code-types
-            '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++" "css"
-              "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
-              "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk" "ditaa"
-              "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
-              "scheme" "sqlite" "html")))
+            '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++"
+			  "css" "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond"
+			  "mscgen" "octave" "oz" "plantuml" "R" "sass" "screen" "sql"
+			  "awk" "ditaa" "haskell" "latex" "lisp" "matlab" "ocaml"
+			  "org" "perl" "ruby" "scheme" "sqlite" "html")))
        (list (ido-completing-read "Source code type: " src-code-types))))
     (progn
       (insert (format "#+BEGIN_SRC %s\n" src-code-type))
@@ -397,7 +397,6 @@
   (enable-emmet-mode)
   (hs-minor-mode))
 
-;; (add-hook 'css-mode-hook 'enable-web-mode)
 (add-hook 'html-mode-hook 'enable-web-mode)
 (add-hook 'nxml-mode-hook 'enable-web-mode)
 (add-hook 'js2-mode-hook 'my-web-mode-hook)
