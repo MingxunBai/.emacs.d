99534f301703def1400135b533518ea0443dd7f3
MingxunBai <mingxunbai@outlook.com>
2017-04-26 19:45:57 +0800

    Update

--- init.el
+++ init.el
@@ -243,6 +243,7 @@
   (setq emmet-self-closing-tag-style ""
         emmet-move-cursor-between-quotes t)
 
+  (define-key emmet-mode-keymap (kbd "<C-return>") nil)
   (define-key emmet-mode-keymap (kbd "C-M-[") 'emmet-prev-edit-point)
   (define-key emmet-mode-keymap (kbd "C-M-]") 'emmet-next-edit-point))
 
