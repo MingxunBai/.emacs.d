05acb9a293ef5e9fa0ec1e4f9cc4eecdaecd447d
MingxunBai <mingxunbai@outlook.com>
2017-06-09 01:14:44 +0800

    Update

---
+++ plugins/lazy-set-key/lazy-full-bind.el
@@ -0,0 +1,33 @@
+;;; lazy-full-bind.el --- lazy-set-key with full features
+
+;; Copyright (C) 2017 MingxunBai
+
+;; Author: MingxunBai
+;; Keywords: convenience
+;; Version: 1.0
+
+;; This is custom key bind use lazy-set-key.
+
+;;; Code:
+(lazy-set-key '(("C-x p"        .   custom-git-push-current-buffer)
+
+                ;; Origami mode
+                ("<f2>"         .   origami-toggle-node)
+                ("C-c o a"      .   origami-show-only-node)
+                ("C-c o o"      .   origami-open-node-recursively)
+                ("C-c o n"      .   origami-next-fold)
+                ("C-c o p"      .   origami-previous-fold)
+                ("C-c o f"      .   origami-forward-fold-same-level)
+                ("C-c o b"      .   origami-backward-fold-same-level)
+                ("C-c o r"      .   origami-reset)
+
+                ;; Project explorer mode
+                ("<f1>"         .   project-explorer-toggle)
+
+                ;; YASnippet mode
+                ("<C-tab>"      .   yas-ido-expand)
+
+                ;; 五笔输入法
+                (";"            .   chinese-wbim-insert-ascii)))
+
+(provide 'lazy-full-bind)
